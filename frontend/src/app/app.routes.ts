c15650c0ac6300670623ab2f8cc5556e4d97e824
PhysicsIsLove <aa.iitbbs@gmail.com>
2025-03-20 02:18:03 +0530

    ui styles changes and some fixes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { ProjectListComponent } from './features/projects/project-list/project-list.component';
 import { ChatComponent } from './features/chat/chat/chat.component';
+import { LandingPageComponent } from './features/landing-page/landing-page.component';
+import { UserDashboardComponent } from './features/dashboard/user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './features/dashboard/admin-dashboard/admin-dashboard.component';
+import { CreateProjectComponent } from './features/projects/create-project/create-project.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/projects', pathMatch: 'full' },
-    { path: 'projects', component: ProjectListComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: "home", component: LandingPageComponent},
+    { path: "admin-dashboard", component: AdminDashboardComponent},
+    { path: "user-dashboard", component: UserDashboardComponent},
+    { path: "project", component: CreateProjectComponent},
     { path: 'chat/:id', component: ChatComponent },
-    { path: '**', redirectTo: '/projects' }
+    { path: '**', redirectTo: '/home' }
   ];
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://api.example.com',
+    loggingEnabled: false
+  };
+  
