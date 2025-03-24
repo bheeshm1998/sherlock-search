70277e2f6a98eea53b8d068c89ea8c5d8d8a361a
PhysicsIsLove <aa.iitbbs@gmail.com>
2025-03-25 01:52:31 +0530

    fixed the dependencies

--- backend/app/geminitest.py
+++ backend/app/geminitest.py
@@ -1,19 +1,19 @@
-import os
-
-import google.generativeai as genai
-from dotenv import load_dotenv
-load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-genai.configure(api_key=GEMINI_API_KEY)
-
-# Select the generative model
-model = genai.GenerativeModel('models/gemini-1.5-pro')  # Or 'gemini-pro-vision' for image inputs
-
-# Example prompt
-prompt = "Write a short summary of Sherlock Holmes."
-
-# Generate content
-response = model.generate_content(prompt)
-
-# Print the generated text
-print(response.text)
+# import os
+#
+# import google.generativeai as genai
+# from dotenv import load_dotenv
+# load_dotenv()
+# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+# genai.configure(api_key=GEMINI_API_KEY)
+#
+# # Select the generative model
+# model = genai.GenerativeModel('models/gemini-1.5-pro')  # Or 'gemini-pro-vision' for image inputs
+#
+# # Example prompt
+# prompt = "Write a short summary of Sherlock Holmes."
+#
+# # Generate content
+# response = model.generate_content(prompt)
+#
+# # Print the generated text
+# print(response.text)
--- backend/app/plugins/auth.py
+++ backend/app/plugins/auth.py
@@ -1,22 +1,22 @@
-from fastapi import HTTPException, Header
-from firebase_admin import auth
-import firebase_admin
-from firebase_admin import credentials
-
-# Initialize Firebase
-cred = credentials.Certificate("path-to-firebase-adminsdk.json")
-firebase_admin.initialize_app(cred)
-
-# Function to verify users
-def verify_user(authorization: str = Header(...)):
-    try:
-        token = authorization.split("Bearer ")[1]  # Extract token
-        decoded_token = auth.verify_id_token(token)
-        email = decoded_token.get("email")
-
-        if not email.endswith("@payoda.com"):
-            raise HTTPException(status_code=403, detail="Access restricted to payoda.com users")
-
-        return email
-    except Exception:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+# from fastapi import HTTPException, Header
+# from firebase_admin import auth
+# import firebase_admin
+# from firebase_admin import credentials
+#
+# # Initialize Firebase
+# cred = credentials.Certificate("path-to-firebase-adminsdk.json")
+# firebase_admin.initialize_app(cred)
+#
+# # Function to verify users
+# def verify_user(authorization: str = Header(...)):
+#     try:
+#         token = authorization.split("Bearer ")[1]  # Extract token
+#         decoded_token = auth.verify_id_token(token)
+#         email = decoded_token.get("email")
+#
+#         if not email.endswith("@payoda.com"):
+#             raise HTTPException(status_code=403, detail="Access restricted to payoda.com users")
+#
+#         return email
+#     except Exception:
+#         raise HTTPException(status_code=401, detail="Unauthorized")
