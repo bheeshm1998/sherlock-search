ce0a778b22c21fa066cab12a16a0de4f14976fac
PhysicsIsLove <aa.iitbbs@gmail.com>
2025-03-25 02:21:32 +0530

    changed the production environment of the app

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://api.example.com',
+    apiBaseUrl: 'https://sherlock-search-1.onrender.com',
     loggingEnabled: false
   };
   
